74b9812266917148d28e6628a2774406b2b70ac4
Manan Karnik <manankarnik03@gmail.com>
2024-01-10 19:33:29 +0530

    nix: update rust shell

--- shells/rust.nix
+++ shells/rust.nix
@@ -4,8 +4,13 @@ with pkgs;
 
 mkShell rec {
   nativeBuildInputs = [
+    wrapGAppsHook
     pkg-config
   ];
+  shellHook = ''
+    echo ""
+    export XDG_DATA_DIRS="$HOME/.nix-profile/share:/usr/local/share:/usr/share:${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}"
+  '';
   buildInputs = [
     rustup
     udev
@@ -17,6 +22,13 @@ mkShell rec {
     xorg.libXrandr # To use the x11 feature
     libxkbcommon
     wayland # To use the wayland feature
+    cairo
+    gtk3
+    glib
+    gsettings-desktop-schemas
+    lld
+    trunk
+    wasm-bindgen-cli
   ];
   LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
 }
