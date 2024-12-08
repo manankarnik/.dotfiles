271e318a5b36cdce6b42843294798510428b19a6
Manan Karnik <manankarnik03@gmail.com>
2024-12-08 12:26:15 +0530

    nvim: add tailwindcss lsp

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -164,6 +164,7 @@ return {
 			pyright = {},
 			rust_analyzer = {},
 			svelte = {},
+			tailwindcss = {},
 			-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 			--
 			-- Some languages (like typescript) have entire language plugins that can be useful:
