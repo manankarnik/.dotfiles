964a3a192abc1afb1102e5ba98a5e2f7abe219a1
Manan Karnik <manankarnik@gmail.com>
2025-05-10 15:50:55 +0530

    Add MIT License snippet

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -23,6 +23,7 @@ return {
 					"rafamadriz/friendly-snippets",
 					config = function()
 						require("luasnip.loaders.from_vscode").lazy_load()
+						require("luasnip.loaders.from_snipmate").lazy_load()
 						require("luasnip").filetype_extend("vue", { "html" })
 					end,
 				},
